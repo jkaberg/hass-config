7de4e68646926134a08e4f1e62e7d394e714bf7c
Joel Kåberg <joel@kaberg.me>
2022-10-23 06:34:09 +0200

    bump

--- pyscript/scripts/power_ctrl.py
+++ pyscript/scripts/power_ctrl.py
@@ -8,7 +8,7 @@ def reload_priceanalyzer():
 @state_trigger("sensor.accumulated_energy_hourly2")
 def handle_power_tariff(value=None):
     value = float(value)
-    heating_adjust = 1
+    heating_adjust = 2
     temp = 0 if not pyscript.PWR_CTRL else float(pyscript.PWR_CTRL)
 
     def check(n, v=value, t=temp):
@@ -20,14 +20,26 @@ def handle_power_tariff(value=None):
         handle_boiler()
     elif check(8.5):
         handle_ev_charger()
-    elif value == 0: # resume
+    elif value == 0 and input_boolean.away_mode == 'off': # resume
         handle_boiler(binary_sensor.priceanalyzer_is_five_cheapest)
         handle_ev_charger(binary_sensor.priceanalyzer_is_ten_cheapest)
         handle_electric_heating(sensor.priceanalyzer_tr_heim)
 
     pyscript.PWR_CTRL = value
 
+@state_trigger("input_boolean.away_mode")
+def handle_away_mode(value=None):
+    heating_adjust = 4
+
+    if value == 'on':
+        handle_boiler()
+        handle_electric_heating(-abs(heating_adjust))
+    else:
+        handle_boiler(binary_sensor.priceanalyzer_is_ten_cheapest)
+        handle_electric_heating(sensor.priceanalyzer_tr_heim)
+
 @state_trigger("binary_sensor.priceanalyzer_is_five_cheapest")
+@state_active("input_boolean.away_mode == 'off'")
 def handle_boiler(value=None): # on / off
     temp = 75 if value == 'on' else 55
 
@@ -38,13 +50,15 @@ def handle_boiler(value=None): # on / off
                "input_boolean.force_evcharge",
                "input_select.current_easee_charger")
 def handle_ev_charger(value=None): # on / off
+    # .isdigit() == current_easee_charger endret seg
     current = int(input_select.current_easee_charger) if 'on' in [value, input_boolean.force_evcharge] or value.isdigit() else 0
 
     easee.set_charger_max_limit(charger_id='EHCQPVGQ',
                                 current=current)
 
 @state_trigger("sensor.priceanalyzer_tr_heim")
-def handle_electric_heating(value=None, var_name=None):
+@state_active("input_boolean.away_mode == 'off'")
+def handle_electric_heating(value=None):
     value = float(value)
 
     BAD = 25
@@ -59,11 +73,11 @@ def handle_electric_heating(value=None, var_name=None):
                'climate.panelovn_mellom_soverom': SOVEROM,
                'climate.panelovn_litet_soverom': SOVEROM,
                'climate.gulvvarme_bad_1_etg': BAD,
+               'climate.gulvvarme_bad_2_etg': BAD,
                'climate.panasonic_ac': PANELOVN,
                'climate.gulvvarme_stue': GULVVARME,
                'climate.gulvvarme_kjokken': GULVVARME,
                'climate.gulvvarme_tv_stue': GULVVARME,
-               'climate.gulvvarme_bad_2_etg': BAD,
                'climate.panelovn_kontor': PANELOVN}
 
     for heater, temp in heaters.items():
