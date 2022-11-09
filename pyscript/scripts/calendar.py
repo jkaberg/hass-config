458e0d2ac1944f930b7226cdedff60ab224f1cc5
Joel Kåberg <joel@kaberg.me>
2022-11-09 19:42:34 +0100

    bump

--- pyscript/scripts/calendar.py
+++ pyscript/scripts/calendar.py
@@ -1,40 +1,40 @@
 from aiofile import async_open
 
 from uuid import uuid4
-from datetime import date, datetime
+from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 from icalendar import Calendar, Event
 
-@time_trigger("cron(* * * * *)")
-async def cal_to_ical(entity_id="calendar.familie", months=2):
-    calendar = hass.data['calendar'].get_entity(entity_id)
+@state_trigger("calendar.familie")
+async def cal_to_ical(var_name=None, months=2):
+    if var_name: 
+        calendar = hass.data['calendar'].get_entity(var_name)
 
-    if not calendar: return
+        if calendar:
+            start_date = datetime.now()
+            end_date = start_date + relativedelta(months=months)
+            events = calendar.async_get_events(hass, start_date, end_date)
 
-    start_date = datetime.now()
-    end_date = start_date + relativedelta(months=months)
-    events = calendar.async_get_events(hass, start_date, end_date)
+            c = Calendar()
+            c.add('PRODID', calendar.name)
+            c.add('VERSION', '2.0')
+            c.add('X-WR-RELCALID', calendar.name)
+            c.add('X-WR-CALNAME', calendar.name)
+            c.add('X-WR-TIMEZONE', 'Europe/Oslo')
+            c.add('X-AUTHOR', 'https://github.com/jkaberg')
 
-    c = Calendar()
-    c.add('PRODID', calendar.name)
-    c.add('VERSION', '2.0')
-    c.add('X-WR-RELCALID', calendar.name)
-    c.add('X-WR-CALNAME', calendar.name)
-    c.add('X-WR-TIMEZONE', 'Europe/Oslo')
-    c.add('X-AUTHOR', 'https://github.com/jkaberg')
+            for event in events:    
+                e = Event()
+                e.add('description', event.description)
+                e.add('uid', str(uuid4()))
+                e.add('summary', event.summary)
+                e.add('location', event.location)
+                e.add('dtstart', event.start)
+                e.add('dtend', event.end)
+                e.add('dtstamp', start_date)
 
-    for event in events:    
-        e = Event()
-        e.add('description', event.description)
-        e.add('uid', str(uuid4()))
-        e.add('summary', event.summary)
-        e.add('location', event.location)
-        e.add('dtstart', event.start)
-        e.add('dtend', event.end)
-        e.add('dtstamp', start_date)
+                c.add_component(e)
 
-        c.add_component(e)
-
-    async with async_open(f"/config/www/{calendar.name.lower()}.ical", "w+") as f:
-        f.write(c.to_ical().decode("utf-8"))
+            async with async_open(f"/config/www/{calendar.name.lower()}.ical", "w+") as f:
+                f.write(c.to_ical().decode("utf-8"))
