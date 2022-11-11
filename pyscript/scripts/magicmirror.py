5da7150ac9b67d461b1df42e9d8b90553332c75b
Joel Kåberg <joel@kaberg.me>
2022-11-11 20:06:50 +0100

    bump

--- pyscript/scripts/magicmirror.py
+++ pyscript/scripts/magicmirror.py
@@ -1,37 +1,26 @@
 import requests
 from datetime import datetime
 
-@state_trigger("media_player.magicmirror.app_name != 'DashCast' \
-                or media_player.magicmirror != 'idle'")
+@state_trigger("media_player.magicmirror.app_name != 'DashCast'", \
+               "media_player.magicmirror == 'off'")
+@state_active("media_player.magicmirror != 'unavailable'")
 def cast_magicmirror():
     url = pyscript.config.get('global').get('magicmirror_url')
 
     dash_cast.load_url(entity_id='media_player.magicmirror', url=url, force=True)
 
-#state_trigger()
-@time_trigger("once(06:00)", "once(09:00)", "once(16:00)", "once(22:30)")
+@state_trigger("binary_sensor.gang_pir_sensor_occupancy")
 @time_active("range(06:00, 22:30)")
-@state_active("group.someone_home == 'home'")
-def motion_magicmirror(value=None, trigger_time=None):
+@state_active("group.someone_home == 'home' and \
+               media_player.magicmirror.app_name == 'DashCast'")
+def motion_magicmirror(value=None):
     def set_brightness(brightness=0):
         base_url = pyscript.config.get('global').get('magicmirror_url')
-        apikey = pyscript.config.get('global').get('magicmirror_apikey')
+        api_key = pyscript.config.get('global').get('magicmirror_apikey')
         url = f"{base_url}/api/brightness/{brightness}"
 
-        task.executor(requests.get, url, headers={"Authorization": f"apiKey {apikey}"})
+        task.executor(requests.get, url, headers={"Authorization": f"apiKey {api_key}"})
 
-    brightness = 0
-
-    if isinstance(trigger_time.now(), datetime):
-        hour = trigger_time.now().hour
-        
-        if hour in [6, 16]:
-            brightness = 100
-        elif hour == 9:
-            brightness = 50
-        elif hour == 22:
-            brightness = 0
-
-    #brightness = 100 if value == 'on' else 0
+    brightness = 100 if value == 'on' else 0
 
     set_brightness(brightness)
